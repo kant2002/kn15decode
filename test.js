abd72812fc70372b67cef2ec10ab8db2259e1574
Andrey Kurdyumov <kant2002@gmail.com>
2015-10-20 23:54:50 +0600

    Created mocha tests for the KN15 decoding

--- test.js
+++
@@ -1,3 +0,0 @@
-var kn15 = require("./index.js");
-console.log("Decode Section 0: 14352 19081");
-console.log(kn15.decodeSection0(['14352', '19081']));
---
+++ tests/decodeTests.js
@@ -0,0 +1,19 @@
+﻿var kn15 = require("../index.js");
+var assert = require('assert');
+
+describe('Tests decoding', function() {
+    it('Decode code 0', function () {
+        var decodedObject = kn15.decodeSection0(['14352', '19081'])
+        var expected = {
+            riverBasin: '14',
+            postNo: '352',
+            day: 19,
+            term: 8,
+            n: 1,
+            hasSection1: true,
+            hasOtherSections: false,
+            hasSection7: false
+        };
+        assert.deepEqual(decodedObject, expected, "Decoded obejct does not match expectations");
+    });
+})
